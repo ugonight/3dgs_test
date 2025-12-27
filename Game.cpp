e341afd229049d6bbd15961210ea6b6bb66b9b2e
ugonight <amihajime0409@outlook.jp>
2025-12-27 10:23:51 +0900

    initial commit

---
+++ Game.cpp
@@ -0,0 +1,342 @@
+//
+// Game.cpp
+//
+
+#include <D3Dcompiler.h>
+
+#include "pch.h"
+#include "Game.h"
+
+extern void ExitGame() noexcept;
+
+using namespace DirectX;
+
+using Microsoft::WRL::ComPtr;
+
+Game::Game() noexcept(false)
+{
+	m_deviceResources = std::make_unique<DX::DeviceResources>();
+	// TODO: Provide parameters for swapchain format, depth/stencil format, and backbuffer count.
+	//   Add DX::DeviceResources::c_AllowTearing to opt-in to variable rate displays.
+	//   Add DX::DeviceResources::c_EnableHDR for HDR10 display.
+	//   Add DX::DeviceResources::c_ReverseDepth to optimize depth buffer clears for 0 instead of 1.
+	m_deviceResources->RegisterDeviceNotify(this);
+}
+
+Game::~Game()
+{
+	if (m_deviceResources)
+	{
+		m_deviceResources->WaitForGpu();
+	}
+}
+
+// Initialize the Direct3D resources required to run.
+void Game::Initialize(HWND window, int width, int height)
+{
+	m_deviceResources->SetWindow(window, width, height);
+
+	m_deviceResources->CreateDeviceResources();
+	CreateDeviceDependentResources();
+
+	m_deviceResources->CreateWindowSizeDependentResources();
+	CreateWindowSizeDependentResources();
+
+	// TODO: Change the timer settings if you want something other than the default variable timestep mode.
+	// e.g. for 60 FPS fixed timestep update logic, call:
+	/*
+	m_timer.SetFixedTimeStep(true);
+	m_timer.SetTargetElapsedSeconds(1.0 / 60);
+	*/
+}
+
+#pragma region Frame Update
+// Executes the basic game loop.
+void Game::Tick()
+{
+	m_timer.Tick([&]()
+		{
+			Update(m_timer);
+		});
+
+	Render();
+}
+
+// Updates the world.
+void Game::Update(DX::StepTimer const& timer)
+{
+	PIXBeginEvent(PIX_COLOR_DEFAULT, L"Update");
+
+	float elapsedTime = float(timer.GetElapsedSeconds());
+
+	// TODO: Add your game logic here.
+	elapsedTime;
+
+	PIXEndEvent();
+}
+#pragma endregion
+
+#pragma region Frame Render
+// Draws the scene.
+void Game::Render()
+{
+	// Don't try to render anything before the first Update.
+	if (m_timer.GetFrameCount() == 0)
+	{
+		return;
+	}
+
+	// Prepare the command list to render a new frame.
+	m_deviceResources->Prepare();
+	Clear();
+
+	auto commandList = m_deviceResources->GetCommandList();
+	PIXBeginEvent(commandList, PIX_COLOR_DEFAULT, L"Render");
+
+	// TODO: Add your rendering code here.
+	PopulateCommandList();
+
+	PIXEndEvent(commandList);
+
+	// Show the new frame.
+	PIXBeginEvent(m_deviceResources->GetCommandQueue(), PIX_COLOR_DEFAULT, L"Present");
+	m_deviceResources->Present();
+
+	// If using the DirectX Tool Kit for DX12, uncomment this line:
+	// m_graphicsMemory->Commit(m_deviceResources->GetCommandQueue());
+
+	PIXEndEvent(m_deviceResources->GetCommandQueue());
+}
+
+// Helper method to clear the back buffers.
+void Game::Clear()
+{
+	auto commandList = m_deviceResources->GetCommandList();
+	PIXBeginEvent(commandList, PIX_COLOR_DEFAULT, L"Clear");
+
+	// Clear the views.
+	const auto rtvDescriptor = m_deviceResources->GetRenderTargetView();
+	const auto dsvDescriptor = m_deviceResources->GetDepthStencilView();
+
+	commandList->OMSetRenderTargets(1, &rtvDescriptor, FALSE, &dsvDescriptor);
+	commandList->ClearRenderTargetView(rtvDescriptor, Colors::CornflowerBlue, 0, nullptr);
+	commandList->ClearDepthStencilView(dsvDescriptor, D3D12_CLEAR_FLAG_DEPTH, 1.0f, 0, 0, nullptr);
+
+	// Set the viewport and scissor rect.
+	const auto viewport = m_deviceResources->GetScreenViewport();
+	const auto scissorRect = m_deviceResources->GetScissorRect();
+	commandList->RSSetViewports(1, &viewport);
+	commandList->RSSetScissorRects(1, &scissorRect);
+
+	PIXEndEvent(commandList);
+}
+#pragma endregion
+
+#pragma region Message Handlers
+// Message handlers
+void Game::OnActivated()
+{
+	// TODO: Game is becoming active window.
+}
+
+void Game::OnDeactivated()
+{
+	// TODO: Game is becoming background window.
+}
+
+void Game::OnSuspending()
+{
+	// TODO: Game is being power-suspended (or minimized).
+}
+
+void Game::OnResuming()
+{
+	m_timer.ResetElapsedTime();
+
+	// TODO: Game is being power-resumed (or returning from minimize).
+}
+
+void Game::OnWindowMoved()
+{
+	const auto r = m_deviceResources->GetOutputSize();
+	m_deviceResources->WindowSizeChanged(r.right, r.bottom);
+}
+
+void Game::OnDisplayChange()
+{
+	m_deviceResources->UpdateColorSpace();
+}
+
+void Game::OnWindowSizeChanged(int width, int height)
+{
+	if (!m_deviceResources->WindowSizeChanged(width, height))
+		return;
+
+	CreateWindowSizeDependentResources();
+
+	// TODO: Game window is being resized.
+}
+
+// Properties
+void Game::GetDefaultSize(int& width, int& height) const noexcept
+{
+	// TODO: Change to desired default window size (note minimum size is 320x200).
+	width = 800;
+	height = 600;
+}
+#pragma endregion
+
+#pragma region Direct3D Resources
+// These are the resources that depend on the device.
+void Game::CreateDeviceDependentResources()
+{
+	auto device = m_deviceResources->GetD3DDevice();
+
+	// Check Shader Model 6 support
+	D3D12_FEATURE_DATA_SHADER_MODEL shaderModel = { D3D_SHADER_MODEL_6_0 };
+	if (FAILED(device->CheckFeatureSupport(D3D12_FEATURE_SHADER_MODEL, &shaderModel, sizeof(shaderModel)))
+		|| (shaderModel.HighestShaderModel < D3D_SHADER_MODEL_6_0))
+	{
+#ifdef _DEBUG
+		OutputDebugStringA("ERROR: Shader Model 6.0 is not supported!\n");
+#endif
+		throw std::runtime_error("Shader Model 6.0 is not supported!");
+	}
+
+	// If using the DirectX Tool Kit for DX12, uncomment this line:
+	// m_graphicsMemory = std::make_unique<GraphicsMemory>(device);
+
+	// TODO: Initialize device dependent objects here (independent of window size).
+	LoadAssets();
+}
+
+// Allocate all memory resources that change on a window SizeChanged event.
+void Game::CreateWindowSizeDependentResources()
+{
+	// TODO: Initialize windows-size dependent objects here.
+}
+
+void Game::OnDeviceLost()
+{
+	// TODO: Add Direct3D resource cleanup here.
+
+	// If using the DirectX Tool Kit for DX12, uncomment this line:
+	// m_graphicsMemory.reset();
+}
+
+void Game::OnDeviceRestored()
+{
+	CreateDeviceDependentResources();
+
+	CreateWindowSizeDependentResources();
+}
+#pragma endregion
+
+void Game::LoadAssets()
+{
+	auto device = m_deviceResources->GetD3DDevice();
+
+	// Create an empty root signature.
+	{
+		CD3DX12_ROOT_SIGNATURE_DESC rootSignatureDesc;
+		rootSignatureDesc.Init(0, nullptr, 0, nullptr, D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT);
+
+		ComPtr<ID3DBlob> signature;
+		ComPtr<ID3DBlob> error;
+		DX::ThrowIfFailed(D3D12SerializeRootSignature(&rootSignatureDesc, D3D_ROOT_SIGNATURE_VERSION_1, &signature, &error));
+		DX::ThrowIfFailed(device->CreateRootSignature(0, signature->GetBufferPointer(), signature->GetBufferSize(), IID_PPV_ARGS(&m_rootSignature)));
+	}
+
+	// Create the pipeline state, which includes compiling and loading shaders.
+	{
+		ComPtr<ID3DBlob> vertexShader;
+		ComPtr<ID3DBlob> pixelShader;
+
+#if defined(_DEBUG)
+		// Enable better shader debugging with the graphics debugging tools.
+		UINT compileFlags = D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION;
+#else
+		UINT compileFlags = 0;
+#endif
+
+		DX::ThrowIfFailed(D3DCompileFromFile(L"shaders.hlsl", nullptr, nullptr, "VSMain", "vs_5_0", compileFlags, 0, &vertexShader, nullptr));
+		DX::ThrowIfFailed(D3DCompileFromFile(L"shaders.hlsl", nullptr, nullptr, "PSMain", "ps_5_0", compileFlags, 0, &pixelShader, nullptr));
+
+		// Define the vertex input layout.
+		D3D12_INPUT_ELEMENT_DESC inputElementDescs[] =
+		{
+			{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
+			{ "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 }
+		};
+
+		// Describe and create the graphics pipeline state object (PSO).
+		D3D12_GRAPHICS_PIPELINE_STATE_DESC psoDesc = {};
+		psoDesc.InputLayout = { inputElementDescs, _countof(inputElementDescs) };
+		psoDesc.pRootSignature = m_rootSignature.Get();
+		psoDesc.VS = CD3DX12_SHADER_BYTECODE(vertexShader.Get());
+		psoDesc.PS = CD3DX12_SHADER_BYTECODE(pixelShader.Get());
+		psoDesc.RasterizerState = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
+		psoDesc.BlendState = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
+		psoDesc.DepthStencilState.DepthEnable = FALSE;
+		psoDesc.DepthStencilState.StencilEnable = FALSE;
+		psoDesc.SampleMask = UINT_MAX;
+		psoDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+		psoDesc.NumRenderTargets = 1;
+		psoDesc.RTVFormats[0] = m_deviceResources->GetBackBufferFormat();
+		psoDesc.SampleDesc.Count = 1;
+		DX::ThrowIfFailed(device->CreateGraphicsPipelineState(&psoDesc, IID_PPV_ARGS(&m_pipelineState)));
+	}
+
+	// Create the vertex buffer.
+	{
+		// Define the geometry for a triangle.
+		auto output_rect = m_deviceResources->GetOutputSize();
+		auto aspect_ratio = static_cast<float>(output_rect.right - output_rect.left) / static_cast<float>(output_rect.bottom - output_rect.top);
+		Vertex triangleVertices[] =
+		{
+			{ { 0.0f, 0.25f * aspect_ratio, 0.0f }, { 1.0f, 0.0f, 0.0f, 1.0f } },
+			{ { 0.25f, -0.25f * aspect_ratio, 0.0f }, { 0.0f, 1.0f, 0.0f, 1.0f } },
+			{ { -0.25f, -0.25f * aspect_ratio, 0.0f }, { 0.0f, 0.0f, 1.0f, 1.0f } }
+		};
+
+		const UINT vertexBufferSize = sizeof(triangleVertices);
+
+		// Note: using upload heaps to transfer static data like vert buffers is not 
+		// recommended. Every time the GPU needs it, the upload heap will be marshalled 
+		// over. Please read up on Default Heap usage. An upload heap is used here for 
+		// code simplicity and because there are very few verts to actually transfer.
+		auto heapProperties = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD);
+		auto resourceDesc = CD3DX12_RESOURCE_DESC::Buffer(vertexBufferSize);
+		DX::ThrowIfFailed(device->CreateCommittedResource(
+			&heapProperties,
+			D3D12_HEAP_FLAG_NONE,
+			&resourceDesc,
+			D3D12_RESOURCE_STATE_GENERIC_READ,
+			nullptr,
+			IID_PPV_ARGS(&m_vertexBuffer)));
+
+		// Copy the triangle data to the vertex buffer.
+		UINT8* pVertexDataBegin;
+		CD3DX12_RANGE readRange(0, 0);        // We do not intend to read from this resource on the CPU.
+		DX::ThrowIfFailed(m_vertexBuffer->Map(0, &readRange, reinterpret_cast<void**>(&pVertexDataBegin)));
+		memcpy(pVertexDataBegin, triangleVertices, sizeof(triangleVertices));
+		m_vertexBuffer->Unmap(0, nullptr);
+
+		// Initialize the vertex buffer view.
+		m_vertexBufferView.BufferLocation = m_vertexBuffer->GetGPUVirtualAddress();
+		m_vertexBufferView.StrideInBytes = sizeof(Vertex);
+		m_vertexBufferView.SizeInBytes = vertexBufferSize;
+	}
+}
+
+void Game::PopulateCommandList()
+{
+	auto commandList = m_deviceResources->GetCommandList();
+	// Set necessary state.
+	commandList->SetPipelineState(m_pipelineState.Get());
+	commandList->SetGraphicsRootSignature(m_rootSignature.Get());
+	commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	commandList->IASetVertexBuffers(0, 1, &m_vertexBufferView);
+	// Draw the triangle.
+	commandList->DrawInstanced(3, 1, 0, 0);
+}
